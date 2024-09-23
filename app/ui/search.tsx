e9b8af593671d7d5712e81ff289cb7d4e5d9ced1
Ahmed Khan <Ahmed.Khan@teradata.com>
2024-09-23 10:50:48 +0500

    fixed search.tsx to update the url

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,13 +1,30 @@
 'use client';
- 
+
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useDebouncedCallback } from 'use-debounce';
- 
+import { useRouter, useSearchParams, usePathname } from 'next/navigation';
+
 export default function Search({ placeholder }: { placeholder: string }) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
   const handleSearch = useDebouncedCallback((term) => {
+    const params = new URLSearchParams(searchParams);
+
+    // Update the query parameter in the URL
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+
+    // Update the URL without reloading the page
+    router.replace(`${pathname}?${params.toString()}`);
+
     console.log(`Searching... ${term}`);
   }, 300); // Add a debounce delay if needed
- 
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -19,6 +36,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get('query') || ''}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
