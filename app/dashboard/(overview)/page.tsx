14735d78111871e5763899956d405dc34bbe5440
Ahmed Khan <Ahmed.Khan@teradata.com>
2024-10-06 09:32:18 +0500

    feat: add metadata for SEO and social media sharing
    - Added title, description, and Open Graph metadata for improved SEO
    - Implemented metadata for login, dashboard, customers, invoices, and create/edit invoice pages
    - Set up dynamic title templates to avoid repetition in page titles
    - Moved favicon and Open Graph image to the root of the /app folder for automatic usage by Next.js

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -7,6 +7,11 @@ import { Suspense } from "react";
 import { RevenueChartSkeleton } from "@/app/ui/skeletons";
 import CardWrapper from "@/app/ui/dashboard/cards";
 import { CardSkeleton } from "@/app/ui/skeletons";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashbaord',
+};
 
 export default async function Page() {
   return (
